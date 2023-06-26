246ee555e02f9506d77dbfea1309d8ae09a1a222
rubypari <137748419+rubypari@users.noreply.github.com>
2023-06-26 21:22:41 +0530

    Create code10.js

---
+++ basic/practise/code10.js
@@ -0,0 +1,17 @@
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.POSITIVE_INFINITY);
+
+2)code
+console.log(9e4);
+console.log(-9e4);
+
+3) code
+console.log(9e400);
+console.log(-9e400);
+
+4) code
+ console.log(9e400);
+console.log(-9e400);
+
+console.log(Number.MAX_VALUE*2);
+console.log(-Number.MAX_VALUE*2)
