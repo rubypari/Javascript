3201d5b338d919b6b02324ce68e1a9ff162e7510
rubypari <137748419+rubypari@users.noreply.github.com>
2023-06-27 11:09:17 +0530

    Update code12.js

--- basic/practise/code12.js
+++ basic/practise/code12.js
@@ -25,4 +25,41 @@ function outer(){
 }
 const cl= outer();
 cl();
+
+
  4) code
+
+const outer=()=>{
+    return ()=>{
+       console.log("inner called...");
+     }
+   
+}
+const cl= outer();
+cl();
+
+5) code
+
+
+const addcounter=()=>{
+ let counter=0;
+ counter++;
+ return counter;
+}
+console.log(addcounter());
+console.log(addcounter());
+console.log(addcounter());
+
+6) code
+
+onst addcounter=()=>{
+  let counter=0;
+  return ()=>{
+     counter++;
+     return counter;
+  }
+}
+const cl = addcounter();
+console.log(cl());
+console.log(cl());
+
